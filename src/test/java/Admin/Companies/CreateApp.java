95cdbccd56fad74007ef82163d17587369c31ba0
afashokova <afashokova95@gmail.com>
2017-02-03 18:30:18 +0300

    не вводится дата документа

---
+++ src/test/java/Admin/Companies/CreateApp.java
@@ -0,0 +1,86 @@
+package Admin.Companies;
+
+/**
+ * Created by afashokova on 03.02.2017.
+ */
+
+import Elements.*;
+import com.codeborne.selenide.Configuration;
+import org.junit.*;
+import java.io.FileInputStream;
+import jxl.Sheet;
+import jxl.Workbook;
+import com.codeborne.selenide.junit.SoftAsserts;
+import static com.codeborne.selenide.Configuration.AssertionMode.SOFT;
+
+
+public class CreateApp {
+    private final LogIn login = new LogIn();
+    private final LogOut logout = new LogOut();
+    private final Pages companies = new Pages();
+    private final CompanyCard companyCard = new CompanyCard();
+    private Sheet Companies;
+    @Rule
+    public SoftAsserts softAsserts = new SoftAsserts();
+
+    @Before
+    public void switchToSoftAssertionsMode() {
+        Configuration.assertionMode = SOFT;
+    }
+    @Test
+    public void TestCreateApp() throws Exception {
+        FileInputStream fi = new FileInputStream("C:\\Users\\Afashokova\\IdeaProjects\\test1\\data\\login.xls");
+        Workbook inputFile = Workbook.getWorkbook(fi);
+        Companies = inputFile.getSheet(11);
+        int j = Companies.getRows();
+        int k = Companies.getColumns();
+        login.SignIn(1, 1);
+        companies.Companies();
+        String AppType, companyFullName,  companyShortName,  AppKind,  AppClass,  municipality,
+               settlement, phoneNumber, faxNumber, INN, surname, firstName, middleName,
+               bank, BankDetails, status, legalAddress,  actualAddress, ogrnip, okvad,
+               pfrNumber, fssNumber, oktmo, pursuantAuthority, snils, kpp,  employeePosition,
+               documentNumber, documentDate, ogrn;
+        for (int i= 1; i<j; i++){
+            AppType = Companies.getCell(0,i).getContents();
+            companyFullName =Companies.getCell(1,i).getContents();
+            companyShortName=Companies.getCell(2,i).getContents();
+            AppKind = Companies.getCell(3,i).getContents();
+            AppClass = Companies.getCell(4,i).getContents();
+            municipality = Companies.getCell(5,i).getContents();
+            settlement = Companies.getCell(6,i).getContents();
+            phoneNumber=Companies.getCell(7,i).getContents();
+            faxNumber=Companies.getCell(8,i).getContents();
+            INN = Companies.getCell(9,i).getContents();
+            surname = Companies.getCell(10,i).getContents();
+            firstName = Companies.getCell(11,i).getContents();
+            middleName = Companies.getCell(12,i).getContents();
+            bank = Companies.getCell(13,i).getContents();
+            BankDetails = Companies.getCell(14,i).getContents();
+            status = Companies.getCell(15,i).getContents();
+            legalAddress = Companies.getCell(16,i).getContents();
+            actualAddress = Companies.getCell(17,i).getContents();
+            ogrnip = Companies.getCell(18,i).getContents();
+            okvad = Companies.getCell(19,i).getContents();
+            pfrNumber = Companies.getCell(20,i).getContents();
+            fssNumber = Companies.getCell(21,i).getContents();
+            oktmo = Companies.getCell(22,i).getContents();
+            pursuantAuthority = Companies.getCell(23,i).getContents();
+            snils = Companies.getCell(24,i).getContents();
+            kpp = Companies.getCell(25, i).getContents();
+            employeePosition = Companies.getCell(26, i).getContents();
+            documentNumber = Companies.getCell(27, i).getContents();
+            documentDate = Companies.getCell(28, i).getContents();
+            ogrn = Companies.getCell(29,i).getContents();
+            companyCard.AddCompany(AppType, companyFullName,  companyShortName,  AppKind,  AppClass,  municipality,
+                    settlement, phoneNumber, faxNumber, INN, surname, firstName, middleName,
+                    bank, BankDetails, status, legalAddress,  actualAddress, ogrnip, okvad,
+                    pfrNumber, fssNumber, oktmo, pursuantAuthority,snils,kpp,employeePosition,
+                    documentNumber, documentDate, ogrn);
+            companies.Companies();
+        }
+        logout.Signout();
+
+    }
+
+}
--- src/test/java/Elements/CompanyCard.java
+++ src/test/java/Elements/CompanyCard.java
@@ -1,59 +1,101 @@
 package Elements;
 
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.*;
 /**
  * Created by afashokova on 01.02.2017.
  */
 public class CompanyCard {
+    String  fullName = "textarea#fullName",
+            shortName = "input#shortName",
+            Kind = "//div[3]/div/div/button",
+            companyClass = "form#formCompany div:nth-child(6) > div > div > button > span.bs-caret > span",
+            Municipality = "form#formCompany div:nth-child(7) > div > div > button",
+            Settlement = "form#formCompany div:nth-child(8) > div > div > button",
+            Phone = "input#mainPhone",
+            Fax = "input#fax",
+            inn = "input#inn",
+            SecondName = "input#firstName",
+            Name = "input#lastName",
+            MiddleName = "input#middleName",
+            AddBank = "form#formCompany h4 > button",
+            BankName = "form#formCompany div.bank-form > div:nth-child(2) > div > div > button",
+            account = "form#formCompany div.bank-form > div:nth-child(3) > div > input",
+            STATUS = "form#formCompany label.btn.btn-default.btn-sm.active.toggle-off",
+            LegAddress = "textarea#legalAddress",
+            factAddress = "textarea#actualAddress",
+            ogrnIP = "input#ogrnip",
+            OKVAD= "input#okvad",
+            PFR = "input#pfrNumber",
+            FSS = "input#fssNumber",
+            OKTMO = "input#oktmo",
+            Authority = "input#pursuantAuthority";
 
     //Добавление паспорта предприятия
     public CompanyCard AddCompany(String AppType, String companyFullName, String companyShortName, String AppKind, String AppClass, String municipality,
                                   String settlement, String phoneNumber, String faxNumber, String INN, String surname, String firstName, String middleName,
                                   String bank, String BankDetails, String status, String legalAddress, String actualAddress, String ogrnip, String okvad,
-                                  String pursuantAuthority) {
+                                  String pfrNumber, String fssNumber, String oktmo, String pursuantAuthority, String snils, String kpp, String employeePosition,
+                                  String documentNumber, String documentDate, String ogrn) {
         $("div#custom-toolbar div:nth-child(1) > button").click();
-        System.out.println("Нажать кнопку Добавить");
         $(withText(AppType)).click();
-        System.out.println("Выбрать тип АПП");
-        $("textarea#fullName").setValue(companyFullName);
-        System.out.println("Ввести Полное наименование компании");
-        $("input#shortName").setValue(companyShortName);
-        System.out.println("Ввести краткое наименование компании");
-        $("form#formCompany div:nth-child(5) > div > div > button").selectOptionByValue(AppKind);//выбор вида АПП
-        System.out.println("Выбрать вид АПП");
-        $("form#formCompany div:nth-child(6) > div > div > button > span.bs-caret > span").selectOptionByValue(AppClass);
-        System.out.println("Выбрать направление деятельности");
-        $("css=form#formCompany div:nth-child(7) > div > div > button").selectOptionByValue(municipality);
-        System.out.println("Выбрать район");
-        $("form#formCompany div:nth-child(8) > div > div > button").selectOptionByValue(settlement);
-        System.out.println("Выбрать поселение");
-        $("input#mainPhone").setValue(phoneNumber);
+        $(fullName).setValue(companyFullName);
+        $(shortName).setValue(companyShortName);
+        $(By.xpath(Kind)).click();
+        $(withText(AppKind)).click();//выбор вида АПП
+        $(companyClass).click();
+        $(withText(AppClass)).click();
+        $(Municipality).click();
+        $(withText(municipality)).click();
+        $(Settlement).click();
+        $(withText(settlement)).click();
+        $(Phone).setValue(phoneNumber);
         sleep(200);
-        System.out.println("Ввести номер телефона");
-        $("input#fax").setValue(faxNumber);
+        $(Fax).setValue(faxNumber);
         sleep(200);
-        System.out.println("Ввести номер факса");
-        $("input#inn").setValue(INN);
-        System.out.println("Ввести ИНН");
-        $("input#firstname").setValue(surname);
-        System.out.println("Ввести фамилию");
-        $("input#secondName").setValue(firstName);
-        System.out.println("Ввести имя");
-        $("input#middleName").setValue(middleName);
-        System.out.println("Ввести отчество");
-        $("form#formCompany h4 > button").click();
-
-        $("form#formCompany div.bank-form > div:nth-child(2) > div > div > button").selectOptionByValue(bank);
-        $("form#formCompany div.bank-form > div:nth-child(3) > div > input").setValue(BankDetails);
+        $(inn).setValue(INN);
+        sleep(200);
+        $(SecondName).shouldBe(Condition.visible).setValue(surname);
+        $(Name).setValue(firstName);
+        $(MiddleName).setValue(middleName);
+        $(AddBank).click();
+        $(BankName).click();
+        $(withText(bank));
+        $(account).setValue(BankDetails);
         if (status == "Да")
-            $("form#formCompany label.btn.btn-default.btn-sm.active.toggle-off").click();
-        $("textarea#legalAddress").setValue(legalAddress);
-        $("textarea#actualAddress").setValue(actualAddress);
-        $("input#ogrnip").setValue(ogrnip);
-        $("input#okvad").setValue(okvad);
-        $("input#pursuantAuthority").setValue(pursuantAuthority);
+            $(STATUS).click();
+        $(LegAddress).setValue(legalAddress);
+        $(factAddress).setValue(actualAddress);
+        if (AppType.equals("Физическое лицо"))
+            $("input#snils").setValue(snils);
+        else {
+            $(OKVAD).setValue(okvad);
+            $(PFR).setValue(pfrNumber);
+            $(FSS).setValue(fssNumber);
+            $(OKTMO).setValue(oktmo);
+            $(Authority).setValue(pursuantAuthority);
+            if (AppType.equals("Индивидуальный предприниматель"))
+                $(ogrnIP).setValue(ogrnip);
+            else {
+                $("input#ogrn").setValue(ogrn);
+                $("input#kpp").setValue(kpp);
+                $("input#employeePosition").setValue(employeePosition);
+                $("input#documentNumber").setValue(documentNumber);
+                $("form#formCompany div:nth-child(31) > div > input").setValue(documentDate);
+            }
+
+        }
+
+
+
         $("form#formCompany div:nth-child(28) > div > button").click();
+        $("div:nth-child(8) > div > p").shouldHave(text(INN));
+        screenshot("C:\\Users\\Afashokova\\IdeaProjects\\test1\\build\\reports\\tests\\1.png");
+
         return page(CompanyCard.class);
     }
 }
