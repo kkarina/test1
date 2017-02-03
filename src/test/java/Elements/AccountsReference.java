b2cd010da997b697f803b0f124e5cc978e4ba280
afashokova <afashokova95@gmail.com>
2017-02-03 13:55:15 +0300

    3

--- src/test/java/Elements/AccountsReference.java
+++ src/test/java/Elements/AccountsReference.java
@@ -9,6 +9,14 @@ import static com.codeborne.selenide.Selenide.*;
 
 @SuppressWarnings("UnusedReturnValue")
 public class AccountsReference {
+    String  logon = "logonName",
+            Email = "email",
+            employeeList = "(//button[@type='button'])[6]",
+            employeeName = "employee.id",
+            newPass = "newPassword",
+            newPass2 = "newPassword2",
+            saveButton = "button.btn.btn-primary.save-form-btn";
+
     //Открытие справочника учетных записей
     public AccountsReference openAccountsReference() {
         $(By.xpath("//div[2]/div/div/div[2]/div")).click(); // открыть "Справочники профилей"
@@ -19,14 +27,14 @@ public class AccountsReference {
     //Создание новой учетной записи
     public AccountsReference UserCreation(String login, String email, String employee, String Password) {
         $(By.xpath("(//button[@type='button'])[2]")).click();// "добавить"
-        $(By.id("logonName")).setValue(login);
-        $(By.id("email")).setValue(email);
-        $(By.xpath("(//button[@type='button'])[6]")).click(); //открыть выпадающий список сотрудников
-        $(By.id("employee.id")).selectOption(employee);
-        $(By.id("newPassword")).setValue(Password);
-        $(By.id("newPassword2")).setValue(Password);
+        $(By.id(logon)).setValue(login);
+        $(By.id(Email)).setValue(email);
+        $(By.xpath(employeeList)).click(); //открыть выпадающий список сотрудников
+        $(By.id(employeeName)).selectOption(employee);
+        $(By.id(newPass)).setValue(Password);
+        $(By.id(newPass2)).setValue(Password);
         sleep(500);
-        $("button.btn.btn-primary.save-form-btn").click(); //сохранить учетную запись
+        $(saveButton).click(); //сохранить учетную запись
         return page(AccountsReference.class);
     }
 
